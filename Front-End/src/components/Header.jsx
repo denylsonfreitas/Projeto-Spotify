b81acec8033591fdccc272030ad6588475a96375
Deny <denylsonfeitosa@gmail.com>
2025-02-16 22:12:53 -0300

    Refactor Header component: remove user image and search functionality, update logo link

--- Front-End/src/components/Header.jsx
+++ Front-End/src/components/Header.jsx
@@ -1,41 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import logoSpotify from "../assets/logo/spotify-logo.png";
-import user from "../assets/user/user.png";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
-const Header = ({ onSearch }) => {
-  const [searchTerm, setSearchTerm] = useState("");
-
-  const handleSearch = (event) => {
-    const value = event.target.value;
-    setSearchTerm(value);
-    onSearch(value);
-  };
-
+const Header = () => {
   return (
     <div className="header">
-      <a href="/" className="logo">
-        <img src={logoSpotify} alt="Spotify Logo" />
-      </a>
+      <Link to="/">
+        <img src={logoSpotify} alt="Logo do Spotify" />
+      </Link>
 
-      <div className="search">
-        <FontAwesomeIcon icon={faMagnifyingGlass} />
-        <input
-          type="text"
-          placeholder="O que você quer ouvir?"
-          value={searchTerm}
-          onChange={handleSearch}
-        />
-      </div>
-
-      <a href="" className="premium">
-        Premium
-      </a>
-
-      <a href="" className="user">
-        <img src={user} alt="User" />
-      </a>
+      <Link to="/" className="header__link">
+        <h1>Spotify</h1>
+      </Link>
     </div>
   );
 };
